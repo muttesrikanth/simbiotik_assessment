0808ac3482ad2740c311cf75364cdae3cc706568
muttesrikanth <srikanth12cn27@gmail.com>
2025-10-02 15:56:58 +0530

    login service implimented

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../core/services/login-service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule, CommonModule],
@@ -8,7 +10,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.css',
 })
 export class Login {
-  constructor() {}
+  constructor(private _loginService:LoginService,private _router:Router) {}
   public loginForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
@@ -16,6 +18,12 @@ export class Login {
   onSubmit() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this._loginService.userLogin(this.loginForm.value).subscribe((data:any)=>{
+        sessionStorage.setItem('token',data.accessToken),
+        this._router.navigate(['dashboard'])
+      },(err)=>{
+        console.log(err)
+        alert(err.message)})
       this.loginForm.reset();
      
     } else {
