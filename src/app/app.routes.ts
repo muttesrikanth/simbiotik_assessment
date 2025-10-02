20b841c592b057954fc54b999bb26bbffb0db609
muttesrikanth <srikanth12cn27@gmail.com>
2025-10-02 13:59:18 +0530

    error routing implimented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,20 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
+import { Dashboard } from './shared/components/dashboard/dashboard';
+import { Welcome } from './shared/components/welcome/welcome';
+import { Todo } from './practice-tests/todo/todo';
+import { Weather } from './practice-tests/weather/weather';
+import { Maps } from './practice-tests/maps/maps';
+import { Error } from './shared/components/error/error';
 
 export const routes: Routes = [
-    {path:'',component:Login}
+    {path:'',component:Login},
+    {path:'dashboard',component:Dashboard, children:[
+        {path:'',component:Welcome},
+        {path:'todo',component:Todo},
+        {path:'weather',component:Weather},
+        {path:'maps',component:Maps},
+
+    ]},
+    {path:'**',component:Error}
 ];
