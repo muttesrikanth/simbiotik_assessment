90dd4f787ff5229fe78d2aa1ffc383c6275962c5
muttesrikanth <srikanth12cn27@gmail.com>
2025-10-02 23:30:01 +0530

    interceptor fixed for weather

--- src/app/core/interceptors/token-interceptor-interceptor.ts
+++ src/app/core/interceptors/token-interceptor-interceptor.ts
@@ -1,10 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const tokenInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
   const token = sessionStorage.getItem('token');
+  const router=inject(Router)
 
   let request = req;
-  if (token) {
+  if (token && router.url=='/dashboard/todo') {
     request = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
