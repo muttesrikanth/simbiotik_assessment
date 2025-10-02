eb3122be0d81a257581960eabdb14c6d06ccd228
muttesrikanth <srikanth12cn27@gmail.com>
2025-10-02 20:10:40 +0530

    weather service implimented

--- src/app/core/services/weather-service.ts
+++ src/app/core/services/weather-service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  
+  private apiKey = '358a7cbe7380f0bdaeef008aebb8bf94';
+  private weatherApi = 'https://api.openweathermap.org/data/2.5/weather';
+  private forecastApi = 'https://api.openweathermap.org/data/2.5/forecast';
+
+  constructor(private http: HttpClient) {}
+
+  getCurrentWeather(city: string): Observable<any> {
+    return this.http.get(`${this.weatherApi}?q=${city}&units=metric&appid=${this.apiKey}`)
+      .pipe(
+        catchError(err => of({ error: true, message: err.message }))
+      );
+  }
+
+  getForecast(city: string): Observable<any> {
+    return this.http.get(`${this.forecastApi}?q=${city}&units=metric&cnt=32&appid=${this.apiKey}`)
+      .pipe(
+        catchError(err => of({ error: true, message: err.message }))
+      );
+  }
+
 }
